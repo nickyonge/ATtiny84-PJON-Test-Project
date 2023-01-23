03eedd3b9a614f6a75d6b08c220f832c7570f276
Nick Yonge <nick@curiobot.com>
2023-01-23 00:44:17 -0800

    commented out testled

--- include/testled.h
+++ include/testled.h
@@ -1,152 +1,154 @@
-#ifndef TESTLED_H
-#define TESTLED_H
+// commented out, removing FastLED because filesize =_=
 
-#include <ArduinoSTL.h>
-#include <FastLED.h>
-#include <arduino-timer.h>
+// #ifndef TESTLED_H
+// #define TESTLED_H
 
-enum Display
-{
-    TLD_NONE = 0,
+// #include <ArduinoSTL.h>
+// #include <FastLED.h>
+// #include <arduino-timer.h>
 
-    TLD_DEFAULT = 1,
-    TLD_ON = 1,
-    TLD_OFF = 0,
+// enum Display
+// {
+//     TLD_NONE = 0,
 
-    TLD_WHITE = 1,
-    TLD_RED = 2,
-    TLD_YELLOW = 3,
-    TLD_GREEN = 4,
-    TLD_CYAN = 5,
-    TLD_BLUE = 6,
-    TLD_MAGENTA = 7,
+//     TLD_DEFAULT = 1,
+//     TLD_ON = 1,
+//     TLD_OFF = 0,
 
-    TLD_GOOD = 4,
-    TLD_BAD = 2,
-    TLD_WARNING = 3,
-    TLD_READY = 6,
+//     TLD_WHITE = 1,
+//     TLD_RED = 2,
+//     TLD_YELLOW = 3,
+//     TLD_GREEN = 4,
+//     TLD_CYAN = 5,
+//     TLD_BLUE = 6,
+//     TLD_MAGENTA = 7,
 
-    TLD_INTERNALERROR = -1,
+//     TLD_GOOD = 4,
+//     TLD_BAD = 2,
+//     TLD_WARNING = 3,
+//     TLD_READY = 6,
 
-    TLD_INDICATOR = UINT8_MAX,
-};
+//     TLD_INTERNALERROR = -1,
 
-enum Pattern
-{
-    TLP_DEFAULT = 0,
-    TLP_SOLID = 0,
-    TLP_BLINK_SLOW = 1,
-    TLP_BLINK_FAST = 2,
-    TLP_FLASH_SLOW = 3,
-    TLP_FLASH_FAST = 4,
-};
+//     TLD_INDICATOR = UINT8_MAX,
+// };
 
-struct TestLEDRGB
-{
-private:
-    const int _rgbTable[8][3] =
-        {{0, 0, 0},       // black
-         {255, 255, 255}, // white
-         {255, 0, 0},     // red
-         {255, 255, 0},   // yellow
-         {255, 255, 0},   // green
-         {0, 255, 255},   // cyan
-         {0, 0, 255},     // blue
-         {255, 0, 255}};  // magenta
+// enum Pattern
+// {
+//     TLP_DEFAULT = 0,
+//     TLP_SOLID = 0,
+//     TLP_BLINK_SLOW = 1,
+//     TLP_BLINK_FAST = 2,
+//     TLP_FLASH_SLOW = 3,
+//     TLP_FLASH_FAST = 4,
+// };
 
-    byte r;
-    byte g;
-    byte b;
+// struct TestLEDRGB
+// {
+// private:
+//     const int _rgbTable[8][3] =
+//         {{0, 0, 0},       // black
+//          {255, 255, 255}, // white
+//          {255, 0, 0},     // red
+//          {255, 255, 0},   // yellow
+//          {255, 255, 0},   // green
+//          {0, 255, 255},   // cyan
+//          {0, 0, 255},     // blue
+//          {255, 0, 255}};  // magenta
 
-public:
-    TestLEDRGB(bool enabled);
-    TestLEDRGB(byte rgb);
-    TestLEDRGB(int rgb);
-    TestLEDRGB(byte r, byte g, byte b);
-    TestLEDRGB(int r, int g, int b);
-    TestLEDRGB(Display displayState);
-    TestLEDRGB(Display displayState, int indicatorId);
-    TestLEDRGB(Display displayState, bool enabled);
-    TestLEDRGB(Display displayState, int indicatorId, bool enabled);
-};
+//     byte r;
+//     byte g;
+//     byte b;
 
-class TestLED
-{
-private:
-    std::vector<int> usedPins;
-    int pin;
-    int ledCount;
-    bool enabled;
-    int indicatorId;
-    Display display;
-    Pattern pattern;
-    TestLED *self;
+// public:
+//     TestLEDRGB(bool enabled);
+//     TestLEDRGB(byte rgb);
+//     TestLEDRGB(int rgb);
+//     TestLEDRGB(byte r, byte g, byte b);
+//     TestLEDRGB(int r, int g, int b);
+//     TestLEDRGB(Display displayState);
+//     TestLEDRGB(Display displayState, int indicatorId);
+//     TestLEDRGB(Display displayState, bool enabled);
+//     TestLEDRGB(Display displayState, int indicatorId, bool enabled);
+// };
 
-    TestLED(int pin, bool startEnabled, Display displayState, int indicatorId, Pattern patternState);
-    TestLED(int pin, int ledCount, bool startEnabled, Display displayState, int indicatorId, Pattern patternState);
+// class TestLED
+// {
+// private:
+//     std::vector<int> usedPins;
+//     int pin;
+//     int ledCount;
+//     bool enabled;
+//     int indicatorId;
+//     Display display;
+//     Pattern pattern;
+//     TestLED *self;
 
-    void UpdateLED();
-    bool IsBlinking();
-    bool IsSlowBlinking();
-    bool IsFastBlinking();
+//     TestLED(int pin, bool startEnabled, Display displayState, int indicatorId, Pattern patternState);
+//     TestLED(int pin, int ledCount, bool startEnabled, Display displayState, int indicatorId, Pattern patternState);
 
-public:
-    TestLED(int pin);
-    TestLED(int pin, bool startEnabled);
-    TestLED(int pin, Display displayState);
-    TestLED(int pin, Display displayState, int indicatorId);
-    TestLED(int pin, Pattern patternState);
-    TestLED(int pin, bool startEnabled, Display displayState);
-    TestLED(int pin, bool startEnabled, int indicatorId);
-    TestLED(int pin, bool startEnabled, Pattern patternState);
-    TestLED(int pin, Display displayState, Pattern patternState);
-    TestLED(int pin, Pattern patternState, int indicatorId);
-    TestLED(int pin, bool startEnabled, Display displayState, Pattern patternState);
-    TestLED(int pin, bool startEnabled, int indicatorId, Pattern patternState);
+//     void UpdateLED();
+//     bool IsBlinking();
+//     bool IsSlowBlinking();
+//     bool IsFastBlinking();
 
-    TestLED(int pin, int ledCount);
-    TestLED(int pin, int ledCount, bool startEnabled);
-    TestLED(int pin, int ledCount, Display displayState);
-    TestLED(int pin, int ledCount, int indicatorId);
-    TestLED(int pin, int ledCount, Pattern patternState);
-    TestLED(int pin, int ledCount, bool startEnabled, Display displayState);
-    TestLED(int pin, int ledCount, bool startEnabled, int indicatorId);
-    TestLED(int pin, int ledCount, bool startEnabled, Pattern patternState);
-    TestLED(int pin, int ledCount, Display displayState, Pattern patternState);
-    TestLED(int pin, int ledCount, int indicatorId, Pattern patternState);
-    TestLED(int pin, int ledCount, bool startEnabled, Display displayState, Pattern patternState);
-    TestLED(int pin, int ledCount, bool startEnabled, int indicatorId, Pattern patternState);
+// public:
+//     TestLED(int pin);
+//     TestLED(int pin, bool startEnabled);
+//     TestLED(int pin, Display displayState);
+//     TestLED(int pin, Display displayState, int indicatorId);
+//     TestLED(int pin, Pattern patternState);
+//     TestLED(int pin, bool startEnabled, Display displayState);
+//     TestLED(int pin, bool startEnabled, int indicatorId);
+//     TestLED(int pin, bool startEnabled, Pattern patternState);
+//     TestLED(int pin, Display displayState, Pattern patternState);
+//     TestLED(int pin, Pattern patternState, int indicatorId);
+//     TestLED(int pin, bool startEnabled, Display displayState, Pattern patternState);
+//     TestLED(int pin, bool startEnabled, int indicatorId, Pattern patternState);
 
-    TestLED TestLED_Good(int pin);
-    TestLED TestLED_Good(int pin, bool startEnabled);
-    TestLED TestLED_Bad(int pin);
-    TestLED TestLED_Bad(int pin, bool startEnabled);
-    TestLED TestLED_Indicator(int pin, int indicatorId);
-    TestLED TestLED_Indicator(int pin, int indicatorId, bool startEnabled);
+//     TestLED(int pin, int ledCount);
+//     TestLED(int pin, int ledCount, bool startEnabled);
+//     TestLED(int pin, int ledCount, Display displayState);
+//     TestLED(int pin, int ledCount, int indicatorId);
+//     TestLED(int pin, int ledCount, Pattern patternState);
+//     TestLED(int pin, int ledCount, bool startEnabled, Display displayState);
+//     TestLED(int pin, int ledCount, bool startEnabled, int indicatorId);
+//     TestLED(int pin, int ledCount, bool startEnabled, Pattern patternState);
+//     TestLED(int pin, int ledCount, Display displayState, Pattern patternState);
+//     TestLED(int pin, int ledCount, int indicatorId, Pattern patternState);
+//     TestLED(int pin, int ledCount, bool startEnabled, Display displayState, Pattern patternState);
+//     TestLED(int pin, int ledCount, bool startEnabled, int indicatorId, Pattern patternState);
 
-    TestLED TestLED_Good(int pin, int ledCount);
-    TestLED TestLED_Good(int pin, int ledCount, bool startEnabled);
-    TestLED TestLED_Bad(int pin, int ledCount);
-    TestLED TestLED_Bad(int pin, int ledCount, bool startEnabled);
-    TestLED TestLED_Indicator(int pin, int ledCount, int indicatorId);
-    TestLED TestLED_Indicator(int pin, int ledCount, int indicatorId, bool startEnabled);
+//     TestLED TestLED_Good(int pin);
+//     TestLED TestLED_Good(int pin, bool startEnabled);
+//     TestLED TestLED_Bad(int pin);
+//     TestLED TestLED_Bad(int pin, bool startEnabled);
+//     TestLED TestLED_Indicator(int pin, int indicatorId);
+//     TestLED TestLED_Indicator(int pin, int indicatorId, bool startEnabled);
 
-    void ToggleEnabled();
+//     TestLED TestLED_Good(int pin, int ledCount);
+//     TestLED TestLED_Good(int pin, int ledCount, bool startEnabled);
+//     TestLED TestLED_Bad(int pin, int ledCount);
+//     TestLED TestLED_Bad(int pin, int ledCount, bool startEnabled);
+//     TestLED TestLED_Indicator(int pin, int ledCount, int indicatorId);
+//     TestLED TestLED_Indicator(int pin, int ledCount, int indicatorId, bool startEnabled);
 
-    void SetEnabled(bool enabled);
-    void SetDisplay(Display displayState);
-    void SetPattern(Pattern patternState);
-    void SetIndicatorID(int indicatorId);
+//     void ToggleEnabled();
 
-    bool GetEnabled();
-    Display  GetDisplay();
-    Pattern GetPattern();
-    int GetIndicatorID();
-};
+//     void SetEnabled(bool enabled);
+//     void SetDisplay(Display displayState);
+//     void SetPattern(Pattern patternState);
+//     void SetIndicatorID(int indicatorId);
 
-void UpdatePin(int pin);
-void UpdateBlink(bool fast);
-void InitializeTimers();
-void UpdateTimers();
+//     bool GetEnabled();
+//     Display  GetDisplay();
+//     Pattern GetPattern();
+//     int GetIndicatorID();
+// };
 
-#endif
+// void UpdatePin(int pin);
+// void UpdateBlink(bool fast);
+// void InitializeTimers();
+// void UpdateTimers();
+
+// #endif
